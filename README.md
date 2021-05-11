daf0fbfdc2c26093949780742f55634a904d06b1
Alexander Makarov <sam@rmcreative.ru>
2021-05-11 14:16:03 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Database Interoperability - Driver
+
+The driver is a low level database interaction abstraction covering the following:
+
+- Database connection.
+- Performing SQL queries using prepared statements.
+- Transactions.
+
---
+++ src/DriverInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace DbInterop\Driver;
+
+/**
+ * The driver is a low level database interaction abstraction covering the following:
+ *
+ * - Database connection.
+ * - Performing SQL queries using prepared statements.
+ * - Transactions.
+ */
+interface DriverInterface
+{
+    // TODO: gather methods here
+}
